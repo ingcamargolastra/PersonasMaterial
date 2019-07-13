73b970adeedb2a186bc68f58a55dc8bcf81afd92
Andres Camargo <ingcamargolastra@gmail.com>
2019-07-13 16:52:36 -0500

    Proyecto Finalizado

--- app/src/main/java/com/unimagdalena/personasmaterial/AgregarPersona.java
+++ app/src/main/java/com/unimagdalena/personasmaterial/AgregarPersona.java
@@ -1,5 +1,6 @@
 package com.unimagdalena.personasmaterial;
 
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class AgregarPersona extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_persona);
+        fotos = new ArrayList<>();
         fotos.add(R.drawable.images);
         fotos.add(R.drawable.images2);
         fotos.add(R.drawable.images3);
@@ -33,7 +35,7 @@ public class AgregarPersona extends AppCompatActivity {
         Persona p = new Persona(id, foto,nom,apel);
         p.guardar();
         limpiar();
-        Snackbar.make(v,"Persona Guardada Exitosamente", Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(v,R.string.mensaje, Snackbar.LENGTH_SHORT).show();
     }
 
     public void limpiar(View v){
@@ -53,5 +55,10 @@ public class AgregarPersona extends AppCompatActivity {
         return fotos.get(fotoSeleccionada);
     }
 
+    public void onBackPressed(){
+        finish();
+        Intent i = new Intent(AgregarPersona.this, MainActivity.class);
+        startActivity(i);
+    }
 
 }
