9bed75bd5ccd6d63b75319cf46250603b9241ce4
Andres Camargo <ingcamargolastra@gmail.com>
2019-07-19 21:35:33 -0500

    Eliminar y detalle persona terminado

--- app/src/main/java/com/unimagdalena/personasmaterial/DetallePersona.java
+++ app/src/main/java/com/unimagdalena/personasmaterial/DetallePersona.java
@@ -1,13 +1,72 @@
 package com.unimagdalena.personasmaterial;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetallePersona extends AppCompatActivity {
-
+    private TextView nombre, apellido;
+    private ImageView foto;
+    private Bundle bundle;
+    private Intent i;
+    private int fot;
+    private String nom, ape, id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_persona);
+        nombre = findViewById(R.id.lblNombreDetalle);
+        apellido = findViewById(R.id.lbApellidoDetalle);
+        foto = findViewById(R.id.foto);
+        i = getIntent();
+        bundle = i.getBundleExtra("datos");
+        nom = bundle.getString("nombre");
+        ape = bundle.getString("apellido");
+        fot = bundle.getInt("foto");
+        id = bundle.getString("id");
+
+        nombre.setText(nom);
+        apellido.setText(ape);
+        foto.setImageResource(fot);
+    }
+
+    public void onBackPressed(){
+        finish();
+        Intent i = new Intent(DetallePersona.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void eliminar(View v){
+        String positivo, negativo;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.titulo_eliminar);
+        builder.setMessage(R.string.mensaje_eliminar);
+        positivo = getString(R.string.positivo);
+        negativo=getString(R.string.negativo);
+
+        builder.setPositiveButton(positivo, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Persona p = new Persona();
+                p.setId(id);
+                p.eliminar();
+                onBackPressed();
+            }
+        });
+
+        builder.setNegativeButton(negativo, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/unimagdalena/personasmaterial/MainActivity.java
+++ app/src/main/java/com/unimagdalena/personasmaterial/MainActivity.java
@@ -22,7 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdaptadorPersona.OnPersonaClickListener {
     private RecyclerView lstOpciones;
     Intent i;
     private ArrayList<Persona> personas;
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         lstOpciones = findViewById(R.id.lstPersonas);
         personas = new ArrayList<>();
-        final AdaptadorPersona adapter = new AdaptadorPersona(personas);
+        final AdaptadorPersona adapter = new AdaptadorPersona(personas, this);
         llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayout.VERTICAL);
         lstOpciones.setLayoutManager(llm);
@@ -74,4 +74,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onPersonaClick(Persona p) {
+        Intent i = new Intent(MainActivity.this, DetallePersona.class);
+        Bundle b = new Bundle();
+        b.putString("id",p.getId());
+        b.putString("nombre",p.getNombre());
+        b.putString("apellido",p.getApellido());
+        b.putInt("foto",p.getFoto());
+        i.putExtra("datos",b);
+        startActivity(i);
+        finish();
+    }
 }
